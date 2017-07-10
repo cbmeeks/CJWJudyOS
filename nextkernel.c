f6084e5448e9b34a6d689e27fceb90d5e0e97d7c
GradiusLover2000 <salcj7400@gmail.com>
2017-07-10 12:50:51 -0400

    Revise nextkernel
    Needed to rewrite execution and loading code.  I will need help with the next update...

--- nextkernel.c
+++ nextkernel.c
@@ -1,5 +1,7 @@
+//This code is compiler-specific (cc65).
 #include <stdlib.h>
 #include <stdio.h>
+//Define hardware memory io macros.
 #define POKE(addr,val)     (*(unsigned char*) (addr) = (val))
 #define POKEW(addr,val)    (*(unsigned*) (addr) = (val))
 #define PEEK(addr)         (*(unsigned char*) (addr))
@@ -12,19 +14,21 @@ void main (void)
 {
   FILE *f;
   char buf[0x10];
-  POKEW(0x9FFE, &progname);
-  POKEW(0x9FFC, &mem);
-  f = fopen("initprog.seq","r");
+  POKEW(0x8FFE, &progname);
+  POKEW(0x8FFC, &mem);
+  f = fopen("initprog,seq","r");
   fread(progname, 1, 16, f);
   fclose(f);
 
   while (progname[0] != 0x00)
   {
-    snprintf(buf, sizeof(buf), "%s.prg", progname);
+    snprintf(buf, sizeof(buf), "%s,prg", progname);
     f = fopen(buf, "r");
     fread(mem, 1, 16384, f);
     fclose(f);
-    asm("jsr $19c0");
+    //Please help!  Need a program translator (actual-opcode-address-mover) to help programs execute!
+    //I need each opcode that uses an "actual" address to have its addresses moved forward so it is within the range of $19c2 to $59c1, not $07ff!
+    asm("jsr $19c2");
   }
   
   printf("Session closed.");
